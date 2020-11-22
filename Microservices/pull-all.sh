cc533bcece9ba8b2cf2346bea6abf85cc0ef39f4
Aditya Kalia <aditya.kalia@gmail.com>
2020-11-22 23:14:09 +0530

    Updating Labs

---
+++ Microservices/pull-all.sh
@@ -0,0 +1,6 @@
+docker pull vergeops/k8s-rvstore-product-api:1.0
+docker pull vergeops/k8s-rvstore-auth-api:1.0
+docker pull vergeops/k8s-rvstore-order-api:1.0
+docker pull vergeops/k8s-rvstore-order-simulator:1.0
+docker pull vergeops/k8s-rvstore-api-gateway:1.0
+docker pull vergeops/k8s-rvstore-ui:1.0
